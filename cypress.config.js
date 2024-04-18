d0484d4618abaadd637bb9999907415d71f6fdd5
Monshy Tinoco <djmonshy_tinoco@hotmail.com>
2024-04-18 09:23:40 -0500

    Actualización de Cypress y configuraciones

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,15 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  //realiza el # de intentos si la prueba falla
+  retries: 1,
+  //desactiva el reporte de video
+  video: false,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    //cambia el tiempo de espera por default
+    defaultCommandTimeout: 10000
   },
 });
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -16,5 +16,11 @@
 // Import commands.js using ES2015 syntax:
 import './commands'
 
+//Salta los errores que se presenten en el test
+Cypress.on('uncaught:exception', (err, runnable) => {
+    // returning false here prevents Cypress from
+    // failing the test
+    return false
+})
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
